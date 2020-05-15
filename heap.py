b86def481f56a9da0af351b26dc078374534ee04
Epion <epion1334@gmail.com>
2020-05-15 19:42:44 +0200

    [TiWO] [Cycle_2] Test failed - set heap size function

--- heap.py
+++ heap.py
@@ -1,6 +1,7 @@
 class Heap:
     def __init__(self):
         self._heap = []
+        self._size = 0
 
     @staticmethod
     def swap(_heap, i, j):
@@ -8,3 +9,8 @@ class Heap:
         tmp = _heap[i]
         _heap[i] = _heap[j]
         _heap[j] = tmp
+
+    @staticmethod
+    def set_heap_size(_heap):
+        """Set heap size."""
+        pass
--- test_heap.py
+++ test_heap.py
@@ -10,6 +10,15 @@ test_swap_data = {
         'expected_heap': [5, 12, 2, 4]
     },
 }
+tmp_expected_heap = Heap()
+tmp_expected_heap._size = 5
+test_set_heap_size_data = {
+    0: {
+        'input_heap': Heap(),
+        'size': 5,
+        'expected_heap': tmp_expected_heap
+    }
+}
 
 
 @pytest.mark.parametrize("input_heap,i,j,expected_heap", [
@@ -18,3 +27,11 @@ test_swap_data = {
 def test_swap(input_heap, i, j, expected_heap):
     Heap.swap(input_heap, i, j)
     assert input_heap == expected_heap
+
+
+@pytest.mark.parametrize("input_heap,size,expected_heap", [
+    (test_set_heap_size_data[0]['input_heap'], test_set_heap_size_data[0]['size'], test_set_heap_size_data[0]['expected_heap']),
+])
+def test_set_heap_size(input_heap, size, expected_heap):
+    input_heap.set_heap_size(size)
+    assert input_heap._size == expected_heap._size
