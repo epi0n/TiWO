cbf6e85738193f6eb3370ddccb324e0e00517b66
Epion <epion1334@gmail.com>
2020-05-15 19:29:08 +0200

    [TiWO] [Cycle_1] Refactor - swap function

--- test_heap.py
+++ test_heap.py
@@ -7,14 +7,14 @@ test_swap_data = {
         'i': 1,
         'j': 2,
         'input_heap': [5, 2, 12, 4],
-        'output_heap': [5, 12, 2, 4]
+        'expected_heap': [5, 12, 2, 4]
     },
 }
 
 
-@pytest.mark.parametrize("_heap,i,j,expected", [
-    (test_swap_data[0]['input_heap'], test_swap_data[0]['i'], test_swap_data[0]['j'], test_swap_data[0]['output_heap']),
+@pytest.mark.parametrize("input_heap,i,j,expected_heap", [
+    (test_swap_data[0]['input_heap'], test_swap_data[0]['i'], test_swap_data[0]['j'], test_swap_data[0]['expected_heap']),
 ])
-def test_swap(_heap, i, j, expected):
-    Heap.swap(_heap, i, j)
-    assert _heap == expected
+def test_swap(input_heap, i, j, expected_heap):
+    Heap.swap(input_heap, i, j)
+    assert input_heap == expected_heap
